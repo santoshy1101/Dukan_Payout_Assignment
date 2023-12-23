d69e78ec15103f44e1b64c3b3f7bc90afabbe61a
santosh <styadav018@gmail.com>
2023-12-23 12:11:12 +0530

    added dukan payout page

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { Sidebar } from "./Sidebar";
+import { Navbar } from "./Navbar";
+import { Payout } from "./Payout";
+
+export function Dashboard() {
+  return (
+    <Flex w="100%" justifyContent="space-around">
+      <Sidebar />
+      <Box w="calc(100% - 224px)">
+        <Navbar />
+        <Payout />
+      </Box>
+    </Flex>
+  );
+}
